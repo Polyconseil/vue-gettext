ed027c306439ac7c1c84a25d8060656a75f8f4b6
kemar <marc@marcarea.com>
2017-08-03 09:15:46 +0200

    [build] 2.0.22

--- dist/vue-gettext.js
+++ dist/vue-gettext.js
@@ -1,5 +1,5 @@
 /**
- * vue-gettext v2.0.21
+ * vue-gettext v2.0.22
  * (c) 2017 Polyconseil
  * @license MIT
  */
@@ -22,7 +22,8 @@ var plurals = {
 
   getTranslationIndex: function (languageCode, n) {
 
-    n = Number.isNaN(parseInt(n)) ? 1 : parseInt(n);  // Fallback to singular.
+    n = parseInt(n);
+    n = typeof n === 'number' && window.isNaN(n) ? 1 : n;  // Fallback to singular.
 
     // Extract the ISO 639 language code. The ISO 639 standard defines
     // two-letter codes for many languages, and three-letter codes for
