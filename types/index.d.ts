edeba71739ef696ef5c8d3a916df5a2c9e4f29f0
Peter Gaal <jety911@gmail.com>
2018-03-31 17:07:11 +0200

    Add type definitions

---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+import VueGettext from './vue-gettext';
+
+export default VueGettext;
+
+export * from './vue';
+export * from './vue-gettext';
---
+++ types/vue.d.ts
@@ -0,0 +1,24 @@
+import Vue, { VueConfiguration } from 'vue/types/vue';
+
+declare module 'vue/types/vue' {
+	interface ILanguageComponent extends Vue {
+		available: {
+			[key: string]: string;
+		};
+		current: string;
+	}
+
+	interface Vue {
+		$translations: object;
+		$language: ILanguageComponent;
+		$gettext: (msgid: string) => string;
+		$pgettext: (context: string, msgid: string) => string;
+		$ngettext: (msgid: string, plural: string, n: number) => string;
+		$npgettext: (context: string, msgid: string, plural: string, n: number) => string;
+		$gettextInterpolate: (msgid: string, context: object) => string;
+	}
+
+	interface VueConfiguration {
+		language: string;
+	}
+}
