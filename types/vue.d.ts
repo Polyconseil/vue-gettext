1db51d9cd9b060ff141a0f2d3f13b94673bd785a
kemar <marc@marcarea.com>
2019-05-15 08:30:12 +0200

    Use an optional parameter in `gettextInterpolate` signature - Fix #95

--- types/vue.d.ts
+++ types/vue.d.ts
@@ -15,7 +15,7 @@ declare module 'vue/types/vue' {
 		$pgettext: (context: string, msgid: string) => string;
 		$ngettext: (msgid: string, plural: string, n: number) => string;
 		$npgettext: (context: string, msgid: string, plural: string, n: number) => string;
-		$gettextInterpolate: (msgid: string, context: object, disableHtmlEscaping: boolean) => string;
+		$gettextInterpolate: (msgid: string, context: object, disableHtmlEscaping?: boolean) => string;
 	}
 
 	interface VueConfiguration {
