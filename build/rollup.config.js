658044a0b0f120aba74f8f0dde426e84bd64e809
Jan Edmund Lazo <jan.lazo@mail.utoronto.ca>
2019-06-26 22:22:51 -0400

    Upgrade rollup to latest, Remove rollup-watch
    rollup's cli has "--watch" flag and rollup-watch is archived.

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,27 +1,27 @@
 const buble = require('rollup-plugin-buble')
 const commonjs = require('rollup-plugin-commonjs')
 const version = process.env.VERSION || require('../package.json').version
-
+const globals = {
+  vue: 'Vue',
+}
 
 module.exports = {
   input: 'src/index.js',
   output: {
-    file: 'dist/vue-gettext.js',
-    format: 'umd',
-  },
-  name: 'VueGettext',
-  globals: {
-    vue: 'Vue',
-  },
-  external: ['vue'],
-  plugins: [
-    commonjs(),
-    buble(),
-  ],
-  banner:
+    banner:
 `/**
  * vue-gettext v${version}
  * (c) ${new Date().getFullYear()} Polyconseil
  * @license MIT
  */`,
+    file: 'dist/vue-gettext.js',
+    format: 'umd',
+    globals,
+    name: 'VueGettext',
+  },
+  external: Object.keys(globals),
+  plugins: [
+    commonjs(),
+    buble(),
+  ],
 }
